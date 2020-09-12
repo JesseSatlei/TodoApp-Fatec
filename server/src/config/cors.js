e59fcd03787569176d7cbe465504fcd48590c1dd
Jessé Satlei <jessesatlei@hotmail.com>
2020-09-12 10:37:45 -0300

    class 12/09

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
-  res.seader('Access-Control-Allow-Origin', '*');
-  res.seader('Access-Control-Allow-Method', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.seader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Method', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 }
--- web/src/todo/todo.jsx
+++ web/src/todo/todo.jsx
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import PageHeader from '../template/PageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
+const URL = 'http://localhost:3003/api/todos';
+
 export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { description: '', list: [] }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, description: e.target.value })
+  }
+
+  handleAdd() {
+    const description = this.state.description;
+    axios.post(URL, { description })
+      .then(resp => console.log('Este serviço funfa'))
+
+    console.log(this.state.description);
+  }
+
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-        <TodoForm />
+        <TodoForm
+          description={this.state.description}
+          handleAdd={this.handleAdd}
+          handleChange={this.handleChange}
+        />
         <TodoList />
       </div>
     );
