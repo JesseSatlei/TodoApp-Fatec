780d487b401e553086404df0ca3ee79a4d89670d
Jessé Satlei <jessesatlei@hotmail.com>
2020-09-15 18:29:26 -0300

    changes

--- web/src/todo/todo.jsx
+++ web/src/todo/todo.jsx
@@ -11,8 +11,20 @@ export default class Todo extends Component {
   constructor(props) {
     super(props);
     this.state = { description: '', list: [] }
+    this.handleRemove = this.handleRemove.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
+    this.refresh();
+  }
+
+  handleRemove(todo) {
+    axios.delete(`${URL}/${todo._id}`)
+      .then(resp => this.refresh())
+  }
+
+  refresh() {
+    axios.get(`${URL}?sort=-createdAt`)
+    .then(resp => this.setState({...this.state, description: '', list: resp.data}))
   }
 
   handleChange(e) {
@@ -22,9 +34,7 @@ export default class Todo extends Component {
   handleAdd() {
     const description = this.state.description;
     axios.post(URL, { description })
-      .then(resp => console.log('Este serviço funfa'))
-
-    console.log(this.state.description);
+      .then(resp => this.refresh)
   }
 
   render() {
@@ -36,7 +46,10 @@ export default class Todo extends Component {
           handleAdd={this.handleAdd}
           handleChange={this.handleChange}
         />
-        <TodoList />
+        <TodoList
+          list={this.state.list}
+          handleRemove={this.handleRemove}
+        />
       </div>
     );
   }
