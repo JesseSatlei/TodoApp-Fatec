61a51f9574cfffb875d28ca6e422fc4776cd9701
Jessé Satlei <jessesatlei@hotmail.com>
2020-09-15 22:22:07 -0300

    changes 15/09

--- web/src/todo/todo.jsx
+++ web/src/todo/todo.jsx
@@ -11,9 +11,15 @@ export default class Todo extends Component {
   constructor(props) {
     super(props);
     this.state = { description: '', list: [] }
-    this.handleRemove = this.handleRemove.bind(this);
+
     this.handleChange = this.handleChange.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
+    this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
+    this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+
     this.refresh();
   }
 
@@ -22,9 +28,18 @@ export default class Todo extends Component {
       .then(resp => this.refresh())
   }
 
-  refresh() {
-    axios.get(`${URL}?sort=-createdAt`)
-    .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+  refresh(description) {
+    const search = description ? `&description__regex=/${description}/` : ''
+    axios.get(`${URL}?sort=-createdAt${search}`)
+      .then(resp => this.setState({ ...this.state, description: '', list: resp.data }))
+  }
+
+  handleSearch() {
+    this.refresh(this.state.description)
+  }
+
+  handleClear() {
+    this.refresh();
   }
 
   handleChange(e) {
@@ -34,9 +49,18 @@ export default class Todo extends Component {
   handleAdd() {
     const description = this.state.description;
     axios.post(URL, { description })
-      .then(resp => this.refresh)
+      .then(resp => this.refresh())
   }
 
+  handleMarkAsDone(todo) {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+    .then(resp => this.refresh())
+  }
+
+  handleMarkAsPending(todo) {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+      .then(resp => this.refresh())
+  }
   render() {
     return (
       <div>
@@ -45,9 +69,13 @@ export default class Todo extends Component {
           description={this.state.description}
           handleAdd={this.handleAdd}
           handleChange={this.handleChange}
+          handleSearch={this.handleSearch}
+          handleClear={this.handleClear}
         />
         <TodoList
           list={this.state.list}
+          handleMarkAsDone={this.handleMarkAsDone}
+          handleMarkAsPending={this.handleMarkAsPending}
           handleRemove={this.handleRemove}
         />
       </div>
